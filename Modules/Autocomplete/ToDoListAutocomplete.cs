fd9ea71ce420fe40890ba4890c97edca3b35983b
Joshua Bissell <joahgreat12@gmail.com>
2023-08-29 16:43:28 +0100

    Added to-do list autocomplete from channel

---
+++ Modules/Autocomplete/ToDoListAutocomplete.cs
@@ -0,0 +1,27 @@
+﻿using Discord;
+using Discord.Interactions;
+using InnoTasker.Data.ToDo;
+using InnoTasker.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoTasker.Modules.Autocomplete
+{
+    public class ToDoListAutocomplete : AutocompleteHandler
+    {
+        public async override Task<AutocompletionResult> GenerateSuggestionsAsync(IInteractionContext context, IAutocompleteInteraction autocompleteInteraction, IParameterInfo parameter, IServiceProvider services)
+        {
+            IGuildService guildService = services.GetRequiredService<IGuildService>();
+            List<AutocompleteResult> results = new();
+            foreach (ToDoList list in guildService.GetGuildData(context.Guild.Id).Lists)
+            {
+                results.Add(new AutocompleteResult(list.Name, list.Name));
+            }
+            return AutocompletionResult.FromSuccess(results);
+        }
+    }
+}
