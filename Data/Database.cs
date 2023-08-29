56b5951e7be43d76ac0d603e667a61ada5d8269b
Joshua Bissell <joahgreat12@gmail.com>
2023-08-29 13:38:21 +0100

    Added Generic Databases

---
+++ Data/Database.cs
@@ -0,0 +1,95 @@
+﻿using Discord;
+using Discord.Commands;
+using InnoTasker.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoTasker.Data
+{
+    public class Database<TKey, TValue> : Dictionary<TKey, TValue> where TKey : notnull
+    {
+        private readonly ILogger _logger;
+
+        public string Path { get; }
+
+        public Database(IServiceProvider services, string path)
+        {
+            _logger = services.GetRequiredService<ILogger>();
+            Path = path;
+
+            if (!Directory.Exists(Path)) Directory.CreateDirectory(Path);
+
+            _logger.Log(LogSeverity.Info, this, $"Initializing database");
+
+            foreach (string file in Directory.GetFiles(path, "*.json"))
+            {
+                string rawJson = File.ReadAllText(file);
+                string fileName = file.Split("/").Last().Split('.').First();
+                try
+                {
+                    TKey key = (TKey)Convert.ChangeType(fileName, typeof(TKey));
+                    TValue value = JsonConvert.DeserializeObject<TValue>(rawJson);
+                    base.Add(key, value);
+                }
+                catch (Exception ex)
+                {
+                    _logger.Log(LogSeverity.Error, this, $"Error loading file '{fileName}', file skipped", ex);
+                }
+            }
+        }
+
+        public new void Add(TKey key, TValue value)
+        {
+            base.Add(key, value);
+            Save(key);
+            _logger.Log(LogSeverity.Debug, this, $"Added entry '{key}'");
+        }
+
+        public new void Remove(TKey key)
+        {
+            try
+            {
+                base.Remove(key);
+                File.Delete($"{Path}{key}.json");
+                _logger.Log(LogSeverity.Debug, this, $"Remove entry '{key}'");
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(LogSeverity.Error, this, $"Failed to remove entry '{key}'", ex);
+            }
+        }
+
+        public void Save()
+        {
+            _logger.Log(LogSeverity.Info, this, $"Starting database save");
+            int failCount = 0;
+            foreach (TKey key in Keys)
+            {
+                if (!Save(key)) failCount++; 
+            }
+            _logger.Log(LogSeverity.Info, this, $"Successfully saved {Keys.Count-failCount} entries with {failCount} failures");
+        }
+
+        public bool Save(TKey key)
+        {
+            try
+            {
+                TValue value = this[key];
+                string path = $"{Path}{key}.json";
+                string valueJson = JsonConvert.SerializeObject(value);
+                File.WriteAllText(path, valueJson);
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(LogSeverity.Error, this, $"Failed to save entry {key}", ex);
+                return false;
+            }
+            return true;
+        }
+    }
+}
