a39cb7a4c4ec6972f05255578e4a8d02bc4ab7ce
Joshua Bissell <joahgreat12@gmail.com>
2023-08-29 16:41:49 +0100

    Added GuildService missing guilds check

--- Data/GuildData.cs
+++ Data/GuildData.cs
@@ -1,5 +1,6 @@
 ﻿using Discord;
 using InnoTasker.Data.ToDo;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,9 +14,13 @@ namespace InnoTasker.Data
         public ulong ID { get; set; }
         public List<ToDoList> Lists { get; set; } = new();
 
+        [JsonIgnore]
+        public bool IsChecked { get; set; } = false;
+
+        public GuildData() {}
         public GuildData(ulong iD)
         {
-            ID = ID;
+            ID = iD;
         }
 
         public ToDoList GetToDoList(string listName) => Lists.FirstOrDefault(x => x.Name == listName);
--- Services/GuildService.cs
+++ Services/GuildService.cs
@@ -15,27 +15,63 @@ namespace InnoTasker.Services
     public class GuildService : InnoServiceBase, IGuildService
     {
         private readonly GuildDatabase _guildDatabase;
+        private readonly DiscordSocketClient _client;
 
         public GuildService(ILogger logger, GuildDatabase data, DiscordSocketClient client) : base(logger)
         {
             _guildDatabase = data;
+            _client = client;
 
             client.JoinedGuild += OnGuildJoin;
             client.LeftGuild += OnGuildLeave;
+
+            client.Ready += OnClientReady;
+
             _logger.Log(LogSeverity.Info, this, $"GuildService initialized");
+        }
+
+        public async Task OnClientReady()
+        {
+            //Check through to see if theres any guilds we dont have in the database
+            int guildsAdded = 0;
+            int guildsRemoved = 0;
+            foreach (SocketGuild guild in _client.Guilds)
+            {
+                if (_guildDatabase.TryGetValue(guild.Id, out GuildData data))
+                {
+                    data.IsChecked = true;
+                }
+                else
+                {
+                    await OnGuildJoin(guild);
+                    guildsAdded++;
+                }
+            }
+            List<GuildData> toRemove = _guildDatabase.Values.Where(x => !x.IsChecked).ToList();
+            foreach (GuildData data in toRemove)
+            {
+                await OnGuildLeave(data.ID);
+                guildsRemoved++;
+            }
+            await _logger.LogAsync(LogSeverity.Info, this, $"Guild database check completed, added {guildsAdded} missing guilds, removed {guildsRemoved} ");
         }
 
         public async Task OnGuildJoin(SocketGuild guild)
         {
             GuildData newData = new GuildData(guild.Id);
+            newData.IsChecked = true;
             _guildDatabase.Add(guild.Id, newData);
             await _logger.LogAsync(LogSeverity.Debug, this, $"Joined guild '{guild.Id}'");
         }
 
         public async Task OnGuildLeave(SocketGuild guild)
         {
-            _guildDatabase.Remove(guild.Id);
-            await _logger.LogAsync(LogSeverity.Debug, this, $"Left guild '{guild.Id}'");
+            await OnGuildLeave(guild.Id);
+        }
+        public async Task OnGuildLeave(ulong guildID)
+        {
+            _guildDatabase.Remove(guildID);
+            await _logger.LogAsync(LogSeverity.Debug, this, $"Left guild '{guildID}'");
         }
 
         public GuildData GetGuildData(ulong guildID)
@@ -46,6 +82,13 @@ namespace InnoTasker.Services
         public ToDoList GetToDoList(ulong guildID, string listName)
         {
             return GetGuildData(guildID).GetToDoList(listName);
+        }
+
+        public ToDoList? GetToDoListFromChannel(ulong guildID, ulong channelID)
+        {
+            return GetGuildData(guildID).Lists.FirstOrDefault(x => x.ListChannelID == channelID 
+                || x.CommandChannelID == channelID 
+                || x.ForumChannelID == channelID);
         }
     }
 }
