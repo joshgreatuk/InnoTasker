6fc04b65dc7807fc6e27601fec6a9316d347a24e
Joshua Bissell <joahgreat12@gmail.com>
2023-08-29 16:43:18 +0100

    Added basic Preconditions

---
+++ Modules/Preconditions/DoListCommandChannelCheck.cs
@@ -0,0 +1,25 @@
+﻿using Discord;
+using Discord.Interactions;
+using InnoTasker.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoTasker.Modules.Preconditions
+{
+    public class DoListCommandChannelCheck : PreconditionAttribute
+    { 
+        public async override Task<PreconditionResult> CheckRequirementsAsync(IInteractionContext context, ICommandInfo commandInfo, IServiceProvider services)
+        {
+            IGuildService guildService = services.GetRequiredService<IGuildService>();
+            if (guildService.GetToDoListFromChannel(context.Guild.Id, context.Channel.Id) != null)
+            {
+                return PreconditionResult.FromSuccess();
+            }
+            return PreconditionResult.FromError("This command must be done in a to-do list channel");
+        }
+    }
+}
---
+++ Modules/Preconditions/DoListUserPermissionCheck.cs
@@ -0,0 +1,37 @@
+﻿using Discord.Commands;
+using InnoTasker.Data.ToDo;
+using InnoTasker.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoTasker.Modules.Preconditions
+{
+    public class DoListUserPermissionCheck : PreconditionAttribute
+    {
+        public ListUserPermissions permissionRequired;
+
+        public DoListUserPermissionCheck(ListUserPermissions permissionRequired)
+        {
+            this.permissionRequired = permissionRequired;
+        }
+
+        public async override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+        {
+            IGuildService guildService = services.GetRequiredService<IGuildService>();
+            ToDoList? targetList = guildService.GetToDoListFromChannel(context.Guild.Id, context.Channel.Id);
+            if (targetList == null) return PreconditionResult.FromError("This command must be done in a to-do list channel");
+
+            if (!targetList.UserPermissions.TryGetValue(context.User.Id, out ListUserPermissions permissions) || !permissions.HasFlag(permissionRequired))
+            {
+                return PreconditionResult.FromError($"You must need {permissionRequired} permissions to run this command");
+            }
+
+            return PreconditionResult.FromSuccess();
+        }
+    }
+}
