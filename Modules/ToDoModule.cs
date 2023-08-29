6c647d366494d285d5c904c3eb3376eb9283b974
Joshua Bissell <joahgreat12@gmail.com>
2023-08-29 16:43:08 +0100

    Added command modules

---
+++ Modules/ToDoModule.cs
@@ -0,0 +1,15 @@
+﻿using Discord.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoTasker.Modules
+{
+    [Group("to-do", "Commands relating to a to-do list")]
+    public class ToDoModule
+    {
+
+    }
+}
