eaf8bd36f3cebc163692ec91a4ad1a2558f1ffc1
Joshua Bissell <joahgreat12@gmail.com>
2023-09-26 12:35:45 +0100

    Autocompletes

--- Modules/Autocomplete/ToDoListCategoryAutocomplete.cs
+++ Modules/Autocomplete/ToDoListCategoryAutocomplete.cs
@@ -5,14 +5,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InnoTasker.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using InnoTasker.Data.ToDo;
 
 namespace InnoTasker.Modules.Autocomplete
 {
     public class ToDoListCategoryAutocomplete : AutocompleteHandler
     {
-        public override Task<AutocompletionResult> GenerateSuggestionsAsync(IInteractionContext context, IAutocompleteInteraction autocompleteInteraction, IParameterInfo parameter, IServiceProvider services)
+        public async override Task<AutocompletionResult> GenerateSuggestionsAsync(IInteractionContext context, IAutocompleteInteraction autocompleteInteraction, IParameterInfo parameter, IServiceProvider services)
         {
-            throw new NotImplementedException();
+            IGuildService guildService = services.GetRequiredService<IGuildService>();
+            ToDoList list = await guildService.GetToDoListFromChannel(context.Channel.Id);
+            List<AutocompleteResult> suggestions = list.Categories.Select(x => new AutocompleteResult(x, x)).ToList();
+            return AutocompletionResult.FromSuccess(suggestions.Take(25));
         }
     }
 }
--- Modules/Autocomplete/ToDoListItemAutocomplete.cs
+++ Modules/Autocomplete/ToDoListItemAutocomplete.cs
@@ -1,5 +1,8 @@
 ﻿using Discord;
 using Discord.Interactions;
+using InnoTasker.Data.ToDo;
+using InnoTasker.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +13,12 @@ namespace InnoTasker.Modules.Autocomplete
 {
     public class ToDoListItemAutocomplete : AutocompleteHandler
     {
-        public override Task<AutocompletionResult> GenerateSuggestionsAsync(IInteractionContext context, IAutocompleteInteraction autocompleteInteraction, IParameterInfo parameter, IServiceProvider services)
+        public async override Task<AutocompletionResult> GenerateSuggestionsAsync(IInteractionContext context, IAutocompleteInteraction autocompleteInteraction, IParameterInfo parameter, IServiceProvider services)
         {
-            throw new NotImplementedException();
+            IGuildService guildService = services.GetRequiredService<IGuildService>();
+            ToDoList list = await guildService.GetToDoListFromChannel(context.Channel.Id);
+            List<AutocompleteResult> suggestions = list.Items.Where(x => !x.IsComplete).Select(x => new AutocompleteResult(x.ID.ToString(), x.ID)).ToList();
+            return AutocompletionResult.FromSuccess(suggestions.Take(25));
         }
     }
 }
