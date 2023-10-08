090e670b478ed56579f807235bb1e409b617ac19
Joshua Bissell <joahgreat12@gmail.com>
2023-10-08 02:08:19 +0100

    Added info command

--- Modules/InnoTaskerModule.cs
+++ Modules/InnoTaskerModule.cs
@@ -1,4 +1,5 @@
-﻿using Discord.Interactions;
+﻿using Discord;
+using Discord.Interactions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +9,26 @@ using System.Threading.Tasks;
 namespace InnoTasker.Modules
 {
     [Group("innotasker", "Innotasker bot commands")]
-    public class InnoTaskerModule
+    public class InnoTaskerModule : InnoModuleBase
     {
-        
+        [SlashCommand("info", "Shows bot info")]
+        public async Task Info()
+        {
+            List<string> fields = new() 
+            {
+                $"**Version:** {Environment.Version}",
+                $"**Developed by:** {MentionUtils.MentionUser(201640887325949953)}", 
+                $"",
+                $"**Thanks for using my bot!"
+            };
+
+            success = false;
+            EmbedBuilder embed = new EmbedBuilder()
+                .WithTitle("InnoTasker")
+                .WithDescription(string.Join("\n", fields))
+                .WithCurrentTimestamp()
+                .WithColor(Color.DarkBlue);
+            await FollowupAsync(embed: embed.Build());
+        }
     }
 }
